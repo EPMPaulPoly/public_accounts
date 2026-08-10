7113301c8aa77b81451e8c762a7200a4d3b12d2c
EPMPaulPoly <paul.charbonnneau@polymtl.ca>
2026-08-10 17:32:27 -0400

    Allow admin to do lower level activities
    Noticed an issue whereby when the admin tries to create an equation var
    he gets a 403 error. This is because the route has requireAuth and
    requiresRole(user) in the stack. This is not intended behaviour as would
    want admin  to be able to do anything that other users do.
    
    There are 2 ways to fix this:
    
    1. only enbale require auth for the equation vars at which point
    any logged in user can change them. This means if an additional
    user class is added that we didn't want to have that capabilty, then
    we would have to reengineer this
    
    2. add a condition in requireRole that allows admin user to have access
    to any capability of any other role.
    
    The second approach was taken as it seems more generalized. Changes are
    thus
    
    auth.ts
    condition to throw 403 error was changed from role!==user.role
    to user.role!=='admin'&&user.role!==role meaning that the error. A
    comment has been added laying out the different cases. This comment
    should be moved over to a unit test when it's created

--- apps/backend/src/api/middleware/auth.ts
+++ apps/backend/src/api/middleware/auth.ts
@@ -17,8 +17,17 @@ export async function requireAuth(req: Request, res: Response, next: NextFunctio
 export function requireRole(role: string) {
     return (req: Request, res: Response, next: NextFunction) => {
         const user = (req as any).user
+        // TO-DO: Create unit test checking that this is desired behaviour and move this comment block there
+        // if the role is different fom role or different from admin, throw an 403 code
+        // | role      | user.role | A user.role!==admin| B  user.role!==role | A&&B | throw 403| case
+        // | admin     |    admin  |       false        |      false          | false| false    | needs admin, has admin
+        // | user      |    admin  |       false        |      true           | false| false    | needs user, has admin
+        // | user      |     user  |       true         |      false          | false| false    | needs user, has user
+        // | admin     |     user  |       true         |      true           | true | true     | needs admin, has user
+        // | user      | undefined |       true         |      true           | true | true     | needs user, has nothing
+        // | admin     | undefined |       true         |      true           | true | true     | needs admin, has nothing 
 
-        if (user.role !== role) {
+        if (user.role !== role && user.role !== 'admin') {
             return res.status(403).json({ error: 'Forbidden' })
         }
 
