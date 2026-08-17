6adf926135ff7a3063ffaf1d3a366d7f0c3d919a
EPMPaulPoly <paul.charbonnneau@polymtl.ca>
2026-08-17 16:18:51 -0400

    Mod resolution method for migrations
    Migrations relied on __dirname variable which doesn't work when running
    on infrastructure or vite. Would work locally because the moduleResolution
    may be more lenient locally. This commit fixes that.
    
    migrationProvider.ts
    Added MigrationFolder variable that is computed based on fileURLToPath function
    and then provided to the new Kysely migrator function

--- apps/backend/src/db/migrationProvider.ts
+++ apps/backend/src/db/migrationProvider.ts
@@ -2,6 +2,9 @@ import { Migrator, FileMigrationProvider } from 'kysely/migration'
 import path from 'path'
 import { Kysely } from 'kysely'
 import fs from 'node:fs/promises'
+import { fileURLToPath } from 'node:url';
+
+const migrationFolder = fileURLToPath(new URL('../migrations', import.meta.url));
 
 export function createMigrator(db: Kysely<any>) {
     return new Migrator({
@@ -9,7 +12,7 @@ export function createMigrator(db: Kysely<any>) {
         provider: new FileMigrationProvider({
             fs,
             path,
-            migrationFolder: path.join(__dirname, '../../migrations'),
+            migrationFolder: migrationFolder
         }),
     })
 }
